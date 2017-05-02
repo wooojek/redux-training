ef6ee044586cd268c37a0df8a88e3ae3a3f9fbc2
Michal Z <yokomura123@gmail.com>
2017-05-02 12:58:33 +0200

    time to start

--- js/app.jsx
+++ js/app.jsx
@@ -1,2 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    class App extends React.Component {
+        render() {
+            return <div>
+                    Hello
+                </div>
+        }
+    }
+
+    ReactDOM.render(
+        <App />,
+        document.getElementById('app')
+    );
+
+});
