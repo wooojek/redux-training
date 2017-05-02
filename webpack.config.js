a00810452c58f10532774c79bd83d77da5297238
Michal Z <yokomura123@gmail.com>
2017-05-02 12:47:44 +0200

    set up packages

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+//Webpack Config
+module.exports	=	{
+		entry:	"./js/app.jsx",
+		output:	{	filename:	"./js/out.js"	},
+		devServer: {
+			inline: true,
+			contentBase: './',
+			port: 3001
+		},
+		watch:	true,
+		module:	{
+				loaders:	[
+						{
+								test:	/\.jsx$/,		exclude:	/node_modules/,
+								loader:	'babel-loader',
+								query:	{	presets:	['es2015',	'stage-2', 'react']	}
+						},
+				]
+		}
+}
